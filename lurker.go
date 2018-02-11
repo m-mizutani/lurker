ab01a1b22db9fc249430bed3f20e8654b8cfa8a0
Masayoshi Mizutani <mizutani@cookpad.com>
2018-02-11 14:59:45 +0900

    Split handler function to multiple handlers

--- lurker.go
+++ lurker.go
@@ -12,10 +12,11 @@ import (
 )
 
 
-func HandlePacket(packet gopacket.Packet) {
-	// fmt.Println(packet)
+type ArpSpoofer struct {
+}
 
-	arpLayer := packet.Layer(layers.LayerTypeARP)
+func (h *ArpSpoofer) Handle (packet *gopacket.Packet) {
+	arpLayer := (*packet).Layer(layers.LayerTypeARP)
 	if arpLayer != nil {
 		arpPkt, _ := arpLayer.(*layers.ARP)
 
@@ -23,8 +24,14 @@ func HandlePacket(packet gopacket.Packet) {
 			fmt.Println("TODO: do action for arp reply")
 		}
 	}
+}
 
-	tcpLayer := packet.Layer(layers.LayerTypeTCP)
+
+type TcpSpoofer struct {
+}
+
+func (h *TcpSpoofer) Handle (packet *gopacket.Packet) {
+	tcpLayer := (*packet).Layer(layers.LayerTypeTCP)
 	if tcpLayer != nil {
 		tcpPkt, _ := tcpLayer.(*layers.TCP)
 
@@ -33,30 +40,76 @@ func HandlePacket(packet gopacket.Packet) {
 			fmt.Println("TODO: do action for TCP syn packet")
 		}
 	}
-
-	appLayer := packet.ApplicationLayer()
-	if tcpLayer != nil && appLayer != nil {
-		data := appLayer.Payload()
-		fmt.Println(data)
-	}
-	
-	return	
 }
 
 
-func main() {
-	var opts struct {
-		FileName string `short:"r" description:"A pcap file" value-name:"FILE"`
-		DevName string `short:"i" description:"Interface name" value-name:"DEV"`
-		FluentDst string `short:"f" description:"Destination of fluentd logs" value-name:"HOST:PORT"`
+type TcpDataLogger struct {
+}
+
+func (h *TcpDataLogger) Handle (packet *gopacket.Packet) {
+	tcpLayer := (*packet).Layer(layers.LayerTypeTCP)
+	appLayer := (*packet).ApplicationLayer()
+	if tcpLayer != nil && appLayer != nil {
+		data := appLayer.Payload()
+		fmt.Println(data)
+	}	
+}
+
+
+type TcpConnLogger struct {
+}
+
+func (h *TcpConnLogger) Handle (packet *gopacket.Packet) {
+	tcpLayer := (*packet).Layer(layers.LayerTypeTCP)
+	if tcpLayer != nil {
+		tcpPkt, _ := tcpLayer.(*layers.TCP)
+
+		if (tcpPkt.FIN == false && tcpPkt.SYN == true &&
+			tcpPkt.RST == false && tcpPkt.ACK == false) {
+			fmt.Println("TODO: do action for TCP syn packet")
+		}
+	}
+}
+
+
+type PacketHandler struct {
+	DevName string
+
+	// handlers
+	ArpReply *ArpSpoofer
+	TcpAck   *TcpSpoofer
+	TcpData  *TcpDataLogger
+	TcpConn  *TcpConnLogger
+}
+
+func (hdlr *PacketHandler) Read (packet *gopacket.Packet) {
+	// fmt.Println(packet)
+
+	if hdlr.ArpReply != nil {
+		hdlr.ArpReply.Handle(packet)
 	}
 
-	_, err := flags.ParseArgs(&opts, os.Args)
-
-	if err != nil {
-		os.Exit(1)
+	if hdlr.TcpAck != nil {
+		hdlr.TcpAck.Handle(packet)
 	}
 
+	if hdlr.TcpConn != nil {
+		hdlr.TcpConn.Handle(packet)
+	}	
+	
+	if hdlr.TcpData != nil {
+		hdlr.TcpData.Handle(packet)
+	}
+}
+
+
+type Options struct {
+	FileName string `short:"r" description:"A pcap file" value-name:"FILE"`
+	DevName string `short:"i" description:"Interface name" value-name:"DEV"`
+	FluentDst string `short:"f" description:"Destination of fluentd logs" value-name:"HOST:PORT"`
+}
+
+func SetupPcapHandler(opts Options) (*pcap.Handle, error) {
 	var handle *pcap.Handle
 	var pcapErr error
 	
@@ -79,21 +132,41 @@ func main() {
 		handle, pcapErr = pcap.OpenLive(opts.DevName, snapshotLen, promiscuous, timeout)
 	}
 
+	return handle, pcapErr
+}
+
+
+func main() {
+	var opts Options
+	
+	_, ParseErr := flags.ParseArgs(&opts, os.Args)
+	if ParseErr != nil {
+		os.Exit(1)
+	}
+
+	
+	handler := PacketHandler{}
+	
+	pcapHandle, pcapErr := SetupPcapHandler(opts)
 	if pcapErr != nil {
-		log.Fatal(err)
+		log.Fatal(pcapErr)
 		os.Exit(1)
 	}
 	
-	if handle == nil {
+	if pcapHandle == nil {
 		log.Fatal("No available device and pcap file, -i or -r option is mandatory")
 		os.Exit(1)
 	}
 
-	defer handle.Close()
+	defer pcapHandle.Close()
+
+
+	handler.ArpReply = &ArpSpoofer{}
 	
 	// Loop through packets in file
-	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
+	packetSource := gopacket.NewPacketSource(pcapHandle, pcapHandle.LinkType())
 	for packet := range packetSource.Packets() {
-		HandlePacket(packet)
+		handler.Read(&packet)
+		// HandlePacket(packet)
 	}
 }
